5dd7b25332156ff9b38255f8761dcbab62d39416
javacoo <leejinhyun99@naver.com>
2024-09-24 16:49:10 +0900

    refactor: Optimize fetch strategy and clarify entity naming
    - To improve performance degradation caused by N+1 problem, set all @XToOne fetch types to LAZY and added CascadeType.ALL to orderItem and delivery in Order entity.
    - Renamed OrderItems to OrderItem to avoid confusion with the List orderItems.
    - Created association methods for Order and Category.

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,18 +6,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "order_item")
 @Getter @Setter
-public class OrderItems {
+public class OrderItem {
 
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
