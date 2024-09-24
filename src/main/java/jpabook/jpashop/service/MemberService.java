23dfe6cf6b9a0bc57501dbeffb6fda0135ec6a05
javacoo <leejinhyun99@naver.com>
2024-09-24 23:30:34 +0900

    feat: Implement MemberRepository and MemberService, delete MemberRepositoryTest
    - Implemented MemberRepository for database operations
    - Created MemberService for business logic
    - Delete MemberRepositoryTest as it is no longer required

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.reopsitory.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //会員登録
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //重複会員検証
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("もう使用されているIDです。");
+        }
+    }
+
+    //会員照会
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    //一件のみ照会
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
