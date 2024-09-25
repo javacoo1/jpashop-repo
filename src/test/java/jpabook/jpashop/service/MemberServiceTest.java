1b4c0e79dfb9d7c1ec1e10574999628d5c63733b
javacoo <leejinhyun99@naver.com>
2024-09-25 15:55:46 +0900

    test: Add MemberServiceTest and configure in-memory DB for testing
    - Created MemberServiceTest.java to verify the functionality of MemberService
    - Configured in-memory H2 database for testing by adding application.yml in the test resources folder
    - Updated build.gradle to include JVM argument for dynamic agent loading during tests

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.reopsitory.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 会員登録() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("javacoo");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 重複会員除外() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("javacuu");
+
+        Member member2 = new Member();
+        member2.setName("javacuu");
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+}
