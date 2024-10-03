4de3ed71a257ba0bdc2d466adcc8f1369ed1a88d
javacoo <leejinhyun99@naver.com>
2024-10-04 02:22:17 +0900

    refactor: Optimize query by directly fetching DTOs
    -Refactored the API to improve query performance by directly fetching DTOs instead of converting entities to DTOs.
    -Created OrderSimpleQueryDto class to represent the required data.
    -Added findOrderDtos() method in OrderRepository to handle the DTO-based query.
    --JPA did not recognize the package name "jpabook.jpashop", resulting in an error
    -Implemented ordersV4() method in OrderSimpleApiController to utilize the new query
    -

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.reopsitory.OrderRepository;
 import jpabook.jpashop.reopsitory.OrderSearch;
+import jpabook.jpashop.reopsitory.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -67,5 +68,10 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
 
         return result;
+    }
+
+    @GetMapping("api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
     }
 }
--- src/main/java/jpabook/jpashop/reopsitory/OrderRepository.java
+++ src/main/java/jpabook/jpashop/reopsitory/OrderRepository.java
@@ -59,4 +59,14 @@ public class OrderRepository {
                 "join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery(
+                //JPAがjpabook.jpashopを認識できない　→　error
+                "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
+                "from Order o " +
+                "join o.member m " +
+                "join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
 }
