c65570a25abd76c106f69c8cf39981689373dbeb
javacoo <leejinhyun99@naver.com>
2024-10-06 23:33:55 +0900

    feat: Add ordersV4 API to optimize query performance by directly querying DTOs
    -Implement ordersV4() method to enhance query performance by directly
     retrieving DTOs in JPA. Created three supporting classes for this functionality.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.reopsitory.OrderRepository;
 import jpabook.jpashop.reopsitory.OrderSearch;
+import jpabook.jpashop.reopsitory.order.query.OrderQueryDto;
+import jpabook.jpashop.reopsitory.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("api/v1/orders")
     public List<Order> ordersV1() {
@@ -102,4 +105,10 @@ public class OrderApiController {
 
         return result;
     }
+
+    @GetMapping("api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
 }
